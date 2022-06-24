1be863e5a06b470bc1c42448c40bd094a19eaae3
AbhishekB27 <akstyles27@gmail.com>
2022-06-24 12:08:31 +0530

    i sent js object through res

--- index.js
+++ index.js
@@ -10,6 +10,17 @@ app.get('/about',(req,res)=>{
     res.send("<h1 style='color:lawngreen'>Hello This Is About Page.</h1>")
 })
 app.get('/contact',(req,res)=>{
-    res.send("<h1 style='color:pink'>Hello This Is Contact Page.</h1>")
+    res.send([{id:001,
+               street:'prashad farm road, Nakraunda',
+               city:'Dehradun',
+               state:'Uttarakhand',
+               mobile_no:0987654321
+              },
+              { id:002,
+                street:'gularghati road, Balawala',
+                city:'Dehradun',
+                state:'Uttarakhand',
+                mobile_no:0987654675
+               }])
 })
 app.listen(8080)
