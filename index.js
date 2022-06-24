efbe7a1d940e758400b24d0c14f6be6d97ab10fe
AbhishekB27 <akstyles27@gmail.com>
2022-06-24 11:55:30 +0530

    add color in router heading

--- index.js
+++ index.js
@@ -3,13 +3,13 @@ const express = require('express')
 const app = express()
 
 app.get('/',(req,res)=>{
-    res.send("<h1>Hello This Is Home Page.</h1>")
+    res.send("<h1 style='color:limegreen'>Hello This Is Home Page.</h1>")
 });
 
 app.get('/about',(req,res)=>{
-    res.send("<h1>Hello This Is About Page.</h1>")
+    res.send("<h1 style='color:lawngreen'>Hello This Is About Page.</h1>")
 })
 app.get('/contact',(req,res)=>{
-    res.send("<h1>Hello This Is Contact Page.</h1>")
+    res.send("<h1 style='color:pink'>Hello This Is Contact Page.</h1>")
 })
 app.listen(8080)
