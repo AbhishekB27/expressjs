4d107a70a60cbff3772973c35d72f1cf1b8c4e18
AbhishekB27 <akstyles27@gmail.com>
2022-06-27 12:38:41 +0530

    today i did many things from data.json

--- index.js
+++ index.js
@@ -1,18 +1,24 @@
 const express = require('express')
 const app = express()
+const fs = require('fs')
+const path = require('path')
 
-app.get('/Home/product/:name',(req,res)=>{
-    // res.send('Hello I m Home Page.') 
-    res.send(`You searched for ${req.params.name}`)
-    console.log(req.params.name)   
+app.get('/todos',(req,res)=>{
+    const { count } = req.query
+    console.log(req.query)
+    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
+    if(!count){
+        return res.send(data)
+    }
+    else{
+        return res.send(data.slice(0,count))
+    }
 })
-app.get('/About',(req,res)=>{
-    res.send('Hello I m About Page.')    
-})
-app.get('/Contact',(req,res)=>{
-    res.send('Hello I m Contact Page.')  
-    console.log(req.query) // it give you query object  
-    console.log(req.path)
+app.get('/todos/:id',(req,res)=>{
+    const { id } = req.params
+    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
+    return res.send(data.find((item)=>item.id == id))
+
 })
 app.listen(8080,()=>{
     console.log("server start on 8080 port.")
