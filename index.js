ab7e60a21a76b00a13a759e627cd310a0d88189c
AbhishekB27 <akstyles27@gmail.com>
2022-06-28 14:22:04 +0530

    today learn create,delete and post the data in data.json in better way.

--- index.js
+++ index.js
@@ -4,54 +4,54 @@ app.use(express.json()) // it is inbuilt middleware
 const fs = require('fs')
 const path = require('path')
 
-app.get('/todos',(req,res)=>{
-    const { count } = req.query
-    console.log(req.query)
-    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
-    if(!count){
-        return res.send(data)
+//fetch quotes data
+app.get('/quotes',(req,res)=>{
+    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
+    // console.log(data)
+    res.send(data)
+})
+//fetch quotes by id
+app.get('/quotes/:quoteId',(req,res)=>{
+   try {
+    let { quoteId } = req.params
+    console.log(quoteId)
+    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
+    // console.log(data)
+    const quotes = data.find(data=>data.id == quoteId )
+    console.log(quotes)
+    if(quotes){
+        res.send(quotes)
     }
     else{
-        return res.send(data.slice(0,count))
+        res.send('Quote not found enter valid id')
+        console.log('Quote not found enter valid id')
     }
+   } catch (error) {
+    res.send(error.message)
+   }
 })
-app.get('/todos/:id',(req,res)=>{
+app.post('/quotes',(req,res)=>{
     try {
-        const { id } = req.params
-        const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
-    
-        const todo = data.find((item)=>item.id ==  id)
-        
-        if(todo){
-            return res.send(todo)
-        }
-        else{
-            return res.send("Item Not Found")
-        }
+        const data = req.body
+    const quotess = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
+    const quote={
+        id: quotess[quotess.length -1].id + 1,
+        ...data
+    }
+    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...quotess,quote]))
+    res.send(quote)
     } catch (error) {
-        return res.send(error.message)
+        res.send(error.message)
     }
 })
-// for adding new todo
-app.post('/todos',(req,res)=>{
-    const data  = req.body
-    const todos = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
-    const todo = {
-        ...data,
-        id: todos.length + 1
-    }
-    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...todos,todo]))
-    console.log(todo)
-    res.send(todo)
-})
-//delete a todo by id
-app.delete('/todos/:id',(req,res)=>{
-    const {id}  = req.params
-    const todos = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
-    const newTodo = todos.filter(data=>data.id != id)
-    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...newTodo]))
-    // console.log(newTodo)
-    res.send(newTodo)
+
+//delete quotes by id
+app.delete('/quotes/:id',(req,res)=>{
+    const {id} = req.params
+    const quotess = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
+    const quotes = quotess.filter(data=>data.id !=id);
+    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...quotes]))
+    res.send(quotes)
 })
 app.listen(8080,()=>{
     console.log("server start on 8080 port.")
