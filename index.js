b2eb36c55e63ee8c311c0a82c6be841098090f47
AbhishekB27 <akstyles27@gmail.com>
2022-06-28 12:28:23 +0530

    today i did read,write and delete operation with data.json

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+app.use(express.json()) // it is inbuilt middleware
 const fs = require('fs')
 const path = require('path')
 
@@ -15,10 +16,42 @@ app.get('/todos',(req,res)=>{
     }
 })
 app.get('/todos/:id',(req,res)=>{
-    const { id } = req.params
-    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
-    return res.send(data.find((item)=>item.id == id))
-
+    try {
+        const { id } = req.params
+        const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
+    
+        const todo = data.find((item)=>item.id ==  id)
+        
+        if(todo){
+            return res.send(todo)
+        }
+        else{
+            return res.send("Item Not Found")
+        }
+    } catch (error) {
+        return res.send(error.message)
+    }
+})
+// for adding new todo
+app.post('/todos',(req,res)=>{
+    const data  = req.body
+    const todos = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
+    const todo = {
+        ...data,
+        id: todos.length + 1
+    }
+    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...todos,todo]))
+    console.log(todo)
+    res.send(todo)
+})
+//delete a todo by id
+app.delete('/todos/:id',(req,res)=>{
+    const {id}  = req.params
+    const todos = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:"UTF-8"}) )
+    const newTodo = todos.filter(data=>data.id != id)
+    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...newTodo]))
+    // console.log(newTodo)
+    res.send(newTodo)
 })
 app.listen(8080,()=>{
     console.log("server start on 8080 port.")
