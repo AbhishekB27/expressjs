c18e39baf960be39d3019a99fc45230ac7d0a6ef
AbhishekB27 <akstyles27@gmail.com>
2022-06-30 09:32:58 +0530

    i did crud operation on mock_data.json in different way.

--- index.js
+++ index.js
@@ -1,87 +1,78 @@
+const { response } = require('express')
 const express = require('express')
 const app = express()
 app.use(express.json()) // it is inbuilt middleware
 const fs = require('fs')
+const { request } = require('http')
 const path = require('path')
 
 //fetch quotes data
-app.get('/quotes',(req,res)=>{
-    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
-    // console.log(data)
-    res.send(data)
-})
-//fetch quotes by id
-app.get('/quotes/:quoteId',(req,res)=>{
-   try {
-    let { quoteId } = req.params
-    console.log(quoteId)
-    const data = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
-    // console.log(data)
-    const quotes = data.find(data=>data.id == quoteId )
-    console.log(quotes)
-    if(quotes){
-        res.send(quotes)
+let accounts = JSON.parse(fs.readFileSync(path.join(__dirname,'MOCK_DATA.json'),{encoding:'utf-8'}))
+// console.log(accounts)
+//fetch all acounts
+  app.get('/accounts',(request,response)=>{
+    response.json(accounts)
+  });
+  //fetch accounts by id
+  app.get('/accounts/:id',(request,response)=>{
+    const accountId = Number(request.params.id)
+    const getAccount = accounts.find(data=>data.id === accountId)
+    if(getAccount === undefined){
+        response.status(500).send("Account Does Not Exist.")
     }
     else{
-        res.status(500).send('Quote not found enter valid id')
-        console.log('Quote not found enter valid id')
+        console.log(getAccount)
+        console.log(accountId)
+        response.json(getAccount)
     }
-   } catch (error) {
-    res.send(error.message)
-   }
-})
-//add the quote in data.json
-app.post('/quotes',(req,res)=>{
+  })
+  //add account 
+  app.post('/accounts',(request,response)=>{
+    const incomingRequest = request.body
+    // console.log(incomingRequest)
+    let newData = {
+      id: accounts[accounts.length -1].id + 1,
+      ...incomingRequest,
+    }
+    fs.writeFileSync(path.join(__dirname,'MOCK_DATA.json'),JSON.stringify([...accounts,newData]))
+    console.log(newData)
+    response.json(newData)
+  })
+  //update data 
+  app.put('/accounts/:id',(request,response)=>{
     try {
-        const data = req.body
-    const quotess = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
-    const quote={
-        id: quotess[quotess.length -1].id + 1,
-        ...data
-    }
-    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...quotess,quote]))
-    res.send(quote)
+      const accountId = Number(request.params.id)
+      const incomingRequest = request.body
+      const account = accounts.find(data=>data.id == accountId)
+      const index = accounts.indexOf(account)
+      console.log(account)
+      if(!account){
+        response.status(500).json("Account Not Found Please! enter valid id")
+      }
+      else{
+        const updateAccount = {...account,...incomingRequest}
+        accounts[index] = updateAccount;
+        console.log(`${account.first_name}'s Account Updated Successfully.`)
+        fs.writeFileSync(path.join(__dirname,'MOCK_DATA.json'),JSON.stringify([...accounts]));
+        response.send(accounts)
+      }
     } catch (error) {
-        res.send(error.message)
+      response.json(error.message)
     }
-})
-
-//delete quotes by id
-app.delete('/quotes/:id',(req,res)=>{
-    const {id} = req.params
-    const quotess = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) )
-    const quotes = quotess.filter(data=>data.id !=id);
-    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...quotes]))
-    res.send(quotes)
-})
-//update the data of quotes
-app.put('/quotes/:quoteId',(req,res)=>{
-    try {
-        const { quoteId } = req.params
-    const quoteData = req.body
-    // console.log(quoteData)
-    const quotes = JSON.parse( fs.readFileSync(path.join(__dirname,'data.json'),{encoding:'utf-8'}) );
-    // console.log(quotes)
-    const newQuotes = quotes.map((todo)=>{
-        // console.log(todo)
-        if(todo.id == quoteId){
-            return{
-                ...todo,
-                ...quoteData,
-            }
-        }
-        else{
-            return todo
-        }
-    });
-    console.log(newQuotes)
-    fs.writeFileSync(path.join(__dirname,'data.json'),JSON.stringify([...newQuotes]))
-    res.send(newQuotes)
-    console.log('Hello I m put request')
-    } catch (error) {
-        res.send(error.message)
+  })
+  //delete user by id
+  app.delete('/accounts/:id',(request,response)=>{
+    const accountId = Number(request.params.id)
+    const filteredAccount = accounts.filter(data=>data.id != accountId);
+    if(!filteredAccount){
+      response.status(500).send("Account Not Found.")
     }
-})
+    else{
+      accounts = filteredAccount
+      fs.writeFileSync(path.join(__dirname,'MOCK_DATA.json'),JSON.stringify([...accounts]))
+      response.send(accounts)
+    }
+  })
 app.listen(8080,()=>{
     console.log("server start on 8080 port.")
 })
